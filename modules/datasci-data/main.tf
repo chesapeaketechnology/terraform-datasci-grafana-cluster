da9b817f51b2b3fc90df76e6f195763f7e67b3da
Les Stroud <lstroud@ctic-inc.com>
2020-07-24 18:46:35 -0400

    Initial commit for building cluster

---
+++ modules/datasci-data/main.tf
@@ -0,0 +1,52 @@
+provider "azurerm" {
+  version = "~> 2.0"
+  features {}
+  disable_terraform_partner_id = true
+}
+
+data "azurerm_resource_group" "data_resource_group" {
+  name = var.resource_group_name
+}
+
+data "azurerm_virtual_network" "data_net" {
+  name                = var.virtual_network_name
+  resource_group_name = data.azurerm_resource_group.data_resource_group.name
+}
+
+resource "azurerm_postgresql_server" "data-pg" {
+  name                = join("-", ["dbs", var.cluster_name, var.environment, "data"])
+  location            = data.azurerm_resource_group.data_resource_group.location
+  resource_group_name = data.azurerm_resource_group.data_resource_group.name
+
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_password
+
+  sku_name   = "GP_Gen5_2"
+  version    = "11"
+  storage_mb = 640000
+
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = true
+  auto_grow_enabled            = true
+
+  public_network_access_enabled    = true
+  ssl_enforcement_enabled          = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
+}
+
+resource "azurerm_postgresql_database" "snet-db" {
+  name                = "snet"
+  resource_group_name = data.azurerm_resource_group.data_resource_group.name
+  server_name         = azurerm_postgresql_server.data-pg.name
+  charset             = "UTF8"
+  collation           = "English_United States.1252"
+}
+
+resource "azurerm_postgresql_firewall_rule" "data-fw" {
+  count = length(var.allow_access_from)
+  name                = join("-", ["dbfw", var.cluster_name, var.environment, "ds-data", count.index])
+  resource_group_name = data.azurerm_resource_group.data_resource_group.name
+  server_name         = azurerm_postgresql_server.data-pg.name
+  start_ip_address    = var.allow_access_from[count.index].start_address
+  end_ip_address      = var.allow_access_from[count.index].end_address
+}
