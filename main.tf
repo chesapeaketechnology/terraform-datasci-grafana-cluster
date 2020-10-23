4c8f359709a139e9ab01aa633a630140ebf4f00b
Les Stroud <lstroud@ctic-inc.com>
2020-10-22 20:23:36 -0400

    Fixes port doubling

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ module "grafana-server" {
   grafana_db_username  = "${module.grafana-data.administrator_login}@${module.grafana-data.server_name}"
   grafana_db_password  = module.grafana-data.administrator_password
   network_profile_id   = var.network_profile_id
-  datasci_db_host      = "${module.datasci-data.server_fqdn}:5432"
+  datasci_db_host      = module.datasci-data.server_fqdn
   datasci_db_password  = module.datasci-data.administrator_password
   datasci_db_ssl_mode  = "require"
   datasci_db_type      = "postgres"
